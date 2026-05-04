78a2e5b71e93afd61d81c0973d318e47da37ebb6
Said Ibra <name@example.com>
2026-05-03 23:22:17 -0400

    init terraform and plan ver 0.1

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
-  instance_type = "t2.micro"
+  ami = "ami-0a1b6a02658659c2a" # Ubuntu 20.04 LTS // us-east-1
+  instance_type = "t3.micro"
 }
